12dcf91260a00e646bd4da456389704d0d8ae29c
rin8351 <format037@gmail.com>
2023-11-25 00:49:57 +0400

    5454

--- main/views.py
+++ main/views.py
@@ -7,13 +7,12 @@ def index(request):
     current_language = get_language()
     base_dir = os.path.join(settings.BASE_DIR, 'main')
     file_path = os.path.join(base_dir, f'description_{current_language}.txt')
-    #file_path2 = os.path.join(base_dir, f'links_{current_language}.txt')
+    file_path2 = os.path.join(base_dir, f'links_{current_language}.txt')
 
     with open(file_path, 'r', encoding='utf-8') as file:
         description = file.read()
 
-    '''with open(file_path2, 'r', encoding='utf-8') as file:
-        links = file.read()'''
+    with open(file_path2, 'r', encoding='utf-8') as file:
+        links = file.read()
     
-    #return render(request, 'main/index.html', {'description': description}, {'links': links})
-    return render(request, 'main/index.html', {'description': description})
+    return render(request, 'main/index.html', {'description': description}, {'links': links})
