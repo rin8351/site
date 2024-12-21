dd3c555f39064590a94909d6c6ddc97a29fcf96c
Alex <format37@gmail.com>
2024-12-21 15:37:14 +0400

    modified:   chronocast/settings.py 	modified:   main/templates/main/index.html

--- chronocast/settings.py
+++ chronocast/settings.py
@@ -46,10 +46,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
